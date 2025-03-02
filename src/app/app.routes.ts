16a1f8ae9a306b608befa0447e0e5d52e82dc04d
EdRiver8 <dwein08@gmail.com>
2025-03-02 12:56:37 -0500

    Creando la primera ruta con un componente de ejemplo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', // almacenar la ruta raíz localhost:4200/
+        component: ExampleComponent, // componente que se renderizará
+    },
+    {
+        path: 'hello', // almacenar la ruta raíz localhost:4200/hello
+        component: ExampleComponent, // componente que se renderizará
+    },
+];
