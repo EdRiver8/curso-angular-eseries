3a7a3aea35899e9dee85c95b3c943c356fc3b541
EdRiver8 <dwein08@gmail.com>
2025-03-02 09:05:30 -0500

    Repasando tipado datos en typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,43 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'spotify';
+
+  // car: any = {
+  //   brand: 'Ford',
+  //   model: 'Fiesta',
+  //   year: 1999
+  // }
+
+  status: string | number = 'active';
+
+  car: CarModel = {
+    brand: 'Ford',
+    model: 'Fiesta',
+    year: 1999
+  }
+
+  listCars: Array<CarModel> = [
+    {
+      brand: 'Ford',
+      model: 'Fiesta',
+      year: 1999
+    },
+    {
+      brand: 'Chevrolet',
+      model: 'Camaro',
+    },
+    {
+      brand: 'Toyota',
+      model: 'Corolla',
+      year: 2001
+    }
+  ]
+}
+
+// las interfaces son una forma de definir la estructura de un objeto
+// en este caso, la interfaz Car define que un objeto de tipo Car debe tener las propiedades brand, model y year
+interface CarModel {
+  brand: string;
+  model: string;
+  year?: number; // el signo de interrogación indica que la propiedad es opcional
 }
