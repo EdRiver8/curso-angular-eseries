e5238e5f68656551c16d2dc7fa69b8bf068321e0
EdRiver8 <dwein08@gmail.com>
2025-03-02 13:09:41 -0500

    Agregando modulo y componentes compartidos en la subcarpeta 'shared'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
 
 export const routes: Routes = [
-    {
-        path: '', // almacenar la ruta raíz localhost:4200/
-        component: ExampleComponent, // componente que se renderizará
-    },
-    {
-        path: 'hello', // almacenar la ruta raíz localhost:4200/hello
-        component: ExampleComponent, // componente que se renderizará
-    },
+    // {
+    //     path: '', // almacenar la ruta raíz localhost:4200/
+    //     component: ExampleComponent, // componente que se renderizará
+    // },
+    // {
+    //     path: 'hello', // almacenar la ruta raíz localhost:4200/hello
+    //     component: ExampleComponent, // componente que se renderizará
+    // },
 ];
